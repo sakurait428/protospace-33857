754bf18a23504a186c3deec7fec1b923648300a7
sakurait428 <sakurai_t@hotmail.com>
2021-02-02 20:21:52 +0900

    first commit

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :prototype
+
+  validates :text, presence: true
+  validates :user, presence: true
+  validates :prototype, presence: true
+
+end
